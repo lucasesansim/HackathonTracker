bb967334839c32ad5bb8078dc21bdf740379c8fb
lucasesansim <lucasesansim@gmail.com>
2022-11-01 09:54:15 -0300

    Initial implementation of routes, corrected jwtMiddleware inclusion

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\DeveloperController;
+use App\Http\Controllers\HackathonController;
+use App\Http\Controllers\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// TEMPLATE ROUTE
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', [AuthController::class, 'authenticate']);
+Route::post('register', [AuthController::class, 'register']);
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('get-user', [AuthController::class, 'getUser']);
+    Route::get('hackathons', [HackathonController::class, 'list']);
+    Route::get('hackathons/{id}', [HackathonController::class, 'show']);
+    Route::get('topTenDevelopers', [DeveloperController::class, 'listTopTenDevelopers']);
 });
